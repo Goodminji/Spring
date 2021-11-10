69e55dbc1f1c7431eed7d12885db54947992cd8e
minji <tksxk0@gmail.com>
2021-11-10 17:51:04 +0900

    초기셋팅

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdvancedApplication {
-
+//초기 셋팅
 	public static void main(String[] args) {
 		SpringApplication.run(AdvancedApplication.class, args);
 	}
